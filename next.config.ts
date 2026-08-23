d0689482b83a03a28c32edfad78c98ded8aac6cf
Mohamed Siddig <mohamedsiddigelsadig@gmail.com>
2026-08-23 11:48:17 +0200

    Refactor next.config.ts for TypeScript settings

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,14 @@
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  ignoreBuildErrors: true,
-}, 
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
-    images: { 
-      remotePattrtns: [
+  images: {
+    remotePatterns: [
       {
         protocol: "https",
         hostname: "*.supabase.co",
